483fd88694291f43cd8fcb1b44985faef808c026
sedemmorgan <heraldmorgan7@gmail.com>
2025-04-19 06:39:52 +0000

    first commit

---
+++ src/patients.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Patients</title>
+    <link rel="stylesheet" href="./patients.css">
+</head>
+<body>
+    <div class="main">
+        <!-- upper -->
+        <div class="upper">
+            <!-- 01 -->
+            <div class="one">
+                <h3>My</h3>
+                <h3>Hospital Management</h3>
+            </div>
+            <!-- 02 -->
+            <div class="two">
+                <div class="left">
+                <ul>
+                    <a href="./home.php"><li>Home</li></a>
+                    <a href="#"><li>Doctors</li></a>
+                    <a href="#"><li style="background-color: hsl(0, 6%, 72%); color: black;">Patients</li></a>
+                    <a href="#"><li>Appointmnent</li></a>
+                    <a href="#"><li>Diagnosis</li></a>
+                    <a href="#"><li>Prescription</li></a>
+                    <a href="#"><li>Store</li></a>
+                    <a href="#"><li>Reports</li></a>
+                    <a href="#"><li>Settings</li></a>
+                </ul>
+                </div>
+                <div class="right">
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">🏠</a>
+                        </div>
+                    </div>
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">⚙️</a>
+                    </div>  
+                    </div>
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">🔒</a>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+            <!-- 03 -->
+            <div class="three">
+                <ul>
+                    <a href="#"><li>Getting Started</li></a>
+                    <a href="#"><li style="color: rgba(121, 2, 2, 0.808);">Dashboard</li></a>
+                </ul>
+            </div>
+        </div>
+
+        <!-- lower -->
+        <div class="lower">
+            <div class="body">
+                <div class="back">
+                    <div class="left"></div>
+                    <div class="right"></div>
+                </div>
+            </div>
+            <div class="sidebar">
+                <div class="tab">Quick Links</div>
+                <div class="ribbon">
+                    <div class="option"><a href="#">New Appointmnent</a></div>
+                    <div class="option"><a href="#">Edit Appointmnent</a></div>
+                    <div class="option"><a href="#">Delete Appointmnent</a></div>
+                    <div class="option"><a href="#">Pending</a></div>
+                    <div class="option"><a href="#">Preview Appointmnent</a></div>
+                </div>
+                <div class="tab">Patient Details</div>
+                <div class="ribbon">
+                    <div class="option"><a href="#">Patient List</a></div>
+                    <div class="option"><a href="#">Add New Patient Details</a></div>
+                    <div class="option"><a href="#">Edit Patient Details</a></div>
+                    <div class="option"><a href="#">Delete Patient Details</a></div>
+                </div>
+                <div class="tab">Account Details</div>
+                <div class="ribbon">
+                    <div class="option"><a href="#">Patients Payments</a></div>
+                    <div class="option"><a href="#">Vendors Payments</a></div>
+                    <div class="option"><a href="#">Patients Deposits</a></div>
+                    <div class="option"><a href="#">Vendors Deposits</a></div>
+                    <div class="option"><a href="#">Patients Cheques</a></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- lowest -->
+         <div class="lowest">@2025 MyHOSPITAL. All rights reserved.</div>
+    </div>
+</body>
+</html>
