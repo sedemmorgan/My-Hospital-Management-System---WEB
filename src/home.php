51103de4a492f5ebeab23ee3e1e000852eb0374c
sedemmorgan <heraldmorgan7@gmail.com>
2025-04-19 06:37:58 +0000

    first commit

---
+++ src/home.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="./home.css">
+</head>
+<body>
+    <div class="main">
+        <!-- upper -->
+        <div class="upper">
+            <!-- 01 -->
+            <div class="one">
+                <h3>My</h3>
+                <h3>Hospital Management</h3>
+            </div>
+            <!-- 02 -->
+            <div class="two">
+                <div class="left">
+                <ul>
+                    <a href="./home.php"><li style="background-color: hsl(0, 6%, 72%); color: black;">Home</li></a>
+                    <a href="#"><li>Doctors</li></a>
+                    <a href="./patients.php"><li>Patients</li></a>
+                    <a href="#"><li>Appointmnent</li></a>
+                    <a href="#"><li>Diagnosis</li></a>
+                    <a href="#"><li>Prescription</li></a>
+                    <a href="#"><li>Store</li></a>
+                    <a href="#"><li>Reports</li></a>
+                    <a href="#"><li>Settings</li></a>
+                </ul>
+                </div>
+                <div class="right">
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">🏠</a>
+                        </div>
+                    </div>
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">⚙️</a>
+                    </div>  
+                    </div>
+                    <div class="primary-button">
+                        <div class="bg">
+                            <a href="#">🔒</a>
+                        </div>    
+                    </div>
+                </div>
+            </div>
+            <!-- 03 -->
+            <div class="three">
+                <ul>
+                    <a href="#"><li>Getting Started</li></a>
+                    <a href="#"><li style="color: rgba(121, 2, 2, 0.808);">Dashboard</li></a>
+                </ul>
+            </div>
+        </div>
+
+        <!-- lower -->
+        <div class="lower">
+            <div class="body">
+
+                <div class="row">
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+                </div>
+
+                <div class="row">
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+                </div>
+
+                <div class="row">
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+
+                    <a href="#">
+                    <div class="card">
+                        <div class="identity">🎯</div>
+                        <div class="pinned">📌📌</div>
+                    </div>
+                    </a>
+                </div>
+            
+            </div>
+            <div class="sidebar">
+                <div class="tab">👉 Quick Links</div>
+                <div class="ribbon1">
+                    <div class="option"><a href="#">New Appointmnent</a></div>
+                    <div class="option"><a href="#">New Patient</a></div>
+                    <div class="option"><a href="#">Add Prescription</a></div>
+                    <div class="option"><a href="#">View Report</a></div>
+                    <div class="option"><a href="#">Billing</a></div>
+                </div>
+                <div class="tab">💬 Notes</div>
+                <div class="ribbon2">
+                    <div class="option"><a href="#">Support Center</a></div>
+                    <div class="option"><a href="#">How to use it</a></div>
+                    <div class="option"><a href="#">Help Line</a></div>
+                    <div class="option"><a href="#">Customer Testimonials</a></div>
+                </div>
+            </div>
+        </div>
+
+        <!-- lowest -->
+         <div class="lowest">@2025 MyHOSPITAL. All rights reserved.</div>
+    </div>
+</body>
+</html>
